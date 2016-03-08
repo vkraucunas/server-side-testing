0694f217e6eb42064ef79ba23414590ad2a2d51b
Valerie Kraucunas <valeriekraucunas@gmail.com>
2016-03-08 11:27:27 -0700

    get all, get single, and post working

--- src/server/routes/shows.js
+++ src/server/routes/shows.js
@@ -4,10 +4,26 @@ var knex = require('../../../db/knex');
 
 function Shows() {
     return knex('shows');
-};
+}
 
 router.get('/shows', function(req, res, next) {
-  res.status(200).json('testing');
+    Shows().then(function(data) {
+        res.status(200).json(data);
+    });
+});
+
+router.get('/show/:id', function(req, res, next) {
+    Shows().where('id', req.params.id)
+    .then(function(data) {
+        res.status(200).json(data);
+    });
+});
+
+router.post('/shows', function(req, res, next) {
+    Shows().insert({name: req.body.name, channel: req.body.channel, genre: req.body.genre, rating: req.body.rating, explicit: req.body.explicit}, 'id')
+    .then(function(data) {
+        res.status(200).json(data[0]);
+    });
 });
 
 module.exports = router;
--- test/apiTest.js
+++ test/apiTest.js
@@ -52,5 +52,62 @@ describe('API routes', function() {
 
     });
 
+    describe('Get a single show', function() {
+        it('should return a single show\' information', function(done) {
+            chai.request(server)
+            .get('/api/show/2')
+            .end(function(err, res) {
+                res.should.have.status(200);
+                res.should.be.json;
+                res.body.should.be.a('array');
+                res.body.length.should.equal(1);
+                res.body[0].should.have.property('name');
+                res.body[0].name.should.equal('Game of Thrones');
+                res.body[0].should.have.property('channel');
+                res.body[0].channel.should.equal('HBO');
+                res.body[0].should.have.property('genre');
+                res.body[0].genre.should.equal('Fantasy');
+                res.body[0].should.have.property('rating');
+                res.body[0].rating.should.equal(5);
+                res.body[0].should.have.property('explicit');
+                res.body[0].explicit.should.equal(true);
+                done();
+            });
+        });
+    });
+
+    describe('Add a single show', function() {
+        it('should add a single show', function(done) {
+            chai.request(server)
+            .post('/api/shows')
+            .send({
+                name: 'testing',
+                channel: 'sdlkf',
+                genre: 'greaaaaaat',
+                rating: 1,
+                explicit: false
+            })
+            .end(function(err, res) {
+                chai.request(server)
+                .get('/api/show/'+res.body)
+                .end(function(err, response) {
+                    response.should.have.status(200);
+                    response.body.should.be.a('array');
+                    response.body[0].should.have.property('name');
+                    response.body[0].name.should.equal('testing');
+                    response.body[0].should.have.property('channel');
+                    response.body[0].channel.should.equal('sdlkf');
+                    response.body[0].should.have.property('genre');
+                    response.body[0].genre.should.equal('greaaaaaat');
+                    response.body[0].should.have.property('rating');
+                    response.body[0].rating.should.equal(1);
+                    response.body[0].should.have.property('explicit');
+                    response.body[0].explicit.should.equal(false);
+                    done();
+                });
+            });
+        });
+    });
+
 });
 
